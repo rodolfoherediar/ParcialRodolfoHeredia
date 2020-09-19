4f3a17975316eacde277f0837e0f402ba7ae786f
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-19 12:04:03 -0400

    Se agrego Controlador y DataContext

---
+++ ParcialRodolfoHeredia/ParcialRodolfoHeredia/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ParcialRodolfoHeredia.Models
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ParcialRodolfoHeredia.Models.Country> Countries { get; set; }
+    }
+}
